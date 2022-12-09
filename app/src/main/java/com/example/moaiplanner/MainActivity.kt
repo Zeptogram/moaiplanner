b0924654fc7ddef255a0df305632f54f21df04ae
Zeptogram <elio.gargiulo.dev@gmail.com>
2022-12-09 13:32:07 +0100

    Aggiunta una navbar di prova

--- app/src/main/java/com/example/moaiplanner/MainActivity.kt
+++ app/src/main/java/com/example/moaiplanner/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.moaiplanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+
+
+
     }
+
+
+
 }
