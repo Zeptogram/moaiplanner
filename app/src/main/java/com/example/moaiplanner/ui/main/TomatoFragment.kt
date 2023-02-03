347a8f506f7f49fd655ead2cee3cc2965aeafc09
Elio Gargiulo <elio.gargiulo.dev@gmail.com>
2023-02-03 01:05:14 +0100

    Fixed Settings Bug

--- app/src/main/java/com/example/moaiplanner/ui/main/TomatoFragment.kt
+++ app/src/main/java/com/example/moaiplanner/ui/main/TomatoFragment.kt
@@ -75,7 +75,6 @@ class TomatoFragment : Fragment() {
                             }
                         }
                     )
-                    true
                 }
             }
             true
