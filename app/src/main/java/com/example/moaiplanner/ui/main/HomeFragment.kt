46d751ced340b4b7b019d0219210bb2d2730f80e
Stefano Rigato <33328982+ImSteph@users.noreply.github.com>
2023-02-05 01:34:29 +0100

    Aggiunto: - Lista nel RecycleView nell'HomeFragment delle collezioni e note dal Firestore dell'utente
    TODO:
    - Finire RecycleView:
        - OnClick non funziona
        - Sistemare Cards UI
    - Sistemare bug riconoscimento stato di autenticazione durante login

--- app/src/main/java/com/example/moaiplanner/ui/main/HomeFragment.kt
+++ app/src/main/java/com/example/moaiplanner/ui/main/HomeFragment.kt
@@ -1,10 +1,18 @@
 package com.example.moaiplanner.ui.main
 
 import RecyclerViewAdapter
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.util.Log
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,11 +21,26 @@ import com.example.moaiplanner.data.repository.user.AuthRepository
 import com.example.moaiplanner.databinding.HomeFragmentBinding
 import com.example.moaiplanner.databinding.SigninFragmentBinding
 import com.example.moaiplanner.util.ItemsViewModel
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import com.google.firebase.storage.ktx.component1
+import com.google.firebase.storage.ktx.component2
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileDescriptor
+import java.io.FileInputStream
 
 
 class HomeFragment : Fragment() {
     lateinit var binding: HomeFragmentBinding
     lateinit var firebase: AuthRepository
+    private lateinit var storage: FirebaseStorage
+    private lateinit var storageRef: StorageReference
+    private lateinit var userDir: StorageReference
 
     fun newInstance(): HomeFragment? {
         return HomeFragment()
@@ -39,6 +62,11 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = HomeFragmentBinding.inflate(inflater, container, false)
 
+        firebase = AuthRepository(requireActivity().application)
+        storage = Firebase.storage
+        storageRef = storage.reference
+        userDir = storageRef.child("${firebase.getCurretUid()}")
+
         // Inflate il layout per il fragment
         return binding.root
     }
@@ -53,19 +81,76 @@ class HomeFragment : Fragment() {
 
         // ArrayList of class ItemsViewModel
         val data = ArrayList<ItemsViewModel>()
-
-        for (i in 1..9) {
-            data.add(ItemsViewModel("Item " + i))
-        }
-
         // This will pass the ArrayList to our Adapter
-        val adapter = RecyclerViewAdapter(data)
-
+        var adapter = RecyclerViewAdapter(data)
         // Setting the Adapter with the recyclerview
         recyclerview?.adapter = adapter
 
-        // on below line we are adding on item
-        // click listener for our grid view.
+        // Get list of files from Firestore
+        userDir.listAll()
+            .addOnSuccessListener { (items, prefixes) ->
+                prefixes.forEach { prefix ->
+                    Log.d("FIRESTORAGE-PREFIX", prefix.toString())
+                    data.add(ItemsViewModel(prefix.toString().split("/").last()))
+                    prefix.listAll()
+                        .addOnSuccessListener {  (items) ->
+                            items.forEach { item ->
+                                Log.d("FIRESTORAGE-PREFIX-ITEM", item.toString())
+                                // Regex che rimuove avatar e file in più che non servono nelle collections/notes
+                                if (item.toString().split("/").last().contains("^[^.]*\$|.*\\.md\$".toRegex()))
+                                    data.add(ItemsViewModel(item.toString().split("/").last()))
+                            }
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(context, "Error getting files", Toast.LENGTH_SHORT).show()
+                        }
+                }
 
+                items.forEach { item ->
+                    Log.d("FIRESTORAGE-ITEM", item.toString())
+                    if (item.toString().split("/").last().contains("^[^.]*\$|.*\\.md\$".toRegex()))
+                        data.add(ItemsViewModel(item.toString().split("/").last()))
+                }
+            }
+            .addOnFailureListener {
+                Log.d("FIRESTORAGE-ERROR", "Error getting file list")
+                Toast.makeText(context, "Error getting files", Toast.LENGTH_SHORT).show()
+            }
+            .addOnSuccessListener {
+                adapter = RecyclerViewAdapter(data)
+                recyclerview?.adapter = adapter
+            }
+
+        binding.buttonShowall.setOnClickListener {
+            Intent(Intent.ACTION_OPEN_DOCUMENT).also {
+                it.type = "text/markdown"
+                startActivityForResult(it, 0)
+            }
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == 0) {
+            val uri = data?.data
+            Log.d("NOTE URI", uri.toString())
+
+            val fileName = "testInCollection.md"
+            val stream = FileInputStream(uri?.let { context?.contentResolver?.openFileDescriptor(it, "r")?.fileDescriptor ?: FileDescriptor() })
+
+            val noteDir = storageRef.child("${firebase.getCurretUid()}/testCollection/${fileName}")
+            val uploadTask = noteDir.putStream(stream)
+
+            // Register observers to listen for when the download is done or if it fails
+            uploadTask.addOnFailureListener {
+                Toast.makeText(context, "Note upload failed", Toast.LENGTH_SHORT).show()
+                stream.close()
+            }.addOnSuccessListener { taskSnapshot ->
+                Toast.makeText(context, "Note uploaded successful", Toast.LENGTH_SHORT).show()
+                stream.close()
+            }
+        }
+    }
+
+
 }
