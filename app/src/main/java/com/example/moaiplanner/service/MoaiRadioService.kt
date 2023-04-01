93f82c8db0dd13b99df1a12995916a9572dc07d7
Stefano Rigato <33328982+ImSteph@users.noreply.github.com>
2023-04-01 03:52:55 +0200

    Added: - Previous/Play/Pause/Skip to Moai Radio

--- app/src/main/java/com/example/moaiplanner/service/MoaiRadioService.kt
+++ app/src/main/java/com/example/moaiplanner/service/MoaiRadioService.kt
@@ -1,17 +1,16 @@
 package com.example.moaiplanner.service
 
-import android.app.*
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_HIGH
-import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_MUTABLE
+import android.app.Service
 import android.content.ComponentName
 import android.content.Intent
 import android.graphics.BitmapFactory
-import android.media.MediaMetadata
 import android.media.MediaPlayer
 import android.media.MediaPlayer.OnPreparedListener
-import android.media.session.MediaController
-import android.media.session.MediaSession
 import android.media.session.PlaybackState
 import android.os.IBinder
 import android.support.v4.media.MediaMetadataCompat
@@ -22,6 +21,15 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.media.session.MediaButtonReceiver
 import com.example.moaiplanner.R
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 
 
 class MoaiRadioService : Service(), OnPreparedListener {
@@ -31,16 +39,21 @@ class MoaiRadioService : Service(), OnPreparedListener {
 
     // Variabili per Media Player
     private val mediaPlayer = MediaPlayer()
-    private val currentSongId : Int = 0
+    private var currentSongId : Int = 0
     private var mediaSession : MediaSessionCompat? = null
     private var mediaController : MediaControllerCompat? = null
+    private var playlistRadio : IntArray? = null
+    private var numSong = 2
+    private val job = SupervisorJob()
+    private val scope = CoroutineScope(Dispatchers.IO + job)
 
-    // Media Actions
-    private val ACTION_PAUSE = "com.example.moaiplanner.action.PAUSE"
-    private val ACTION_PLAY = "com.example.moaiplanner.action.PLAY"
-    private val ACTION_PLAY_PAUSE = "com.example.moaiplanner.action.PLAY_PAUSE"
-    private val ACTION_NEXT = "com.example.moaiplanner.action.NEXT"
-    private val ACTION_PREV = "com.example.moaiplanner.action.PREV"
+    data class Song(
+        val artist: String,
+        val duration: Long,
+        val id: Int,
+        val link: String,
+        val name: String
+    )
 
     override fun onBind(intent: Intent?): IBinder? {
         return binder
@@ -48,6 +61,9 @@ class MoaiRadioService : Service(), OnPreparedListener {
 
     override fun onCreate() {
         super.onCreate()
+
+        playlistRadio = IntArray(numSong, { it + 1 })
+        playlistRadio?.shuffle()
 
         mediaSession = MediaSessionCompat(applicationContext, "MediaSessionDebug")
         mediaController = mediaSession!!.controller
@@ -57,37 +73,6 @@ class MoaiRadioService : Service(), OnPreparedListener {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.d("PLAYBACK", "START")
-
-        when (intent?.action) {
-            ACTION_PAUSE -> {
-                // Handle pause action
-                Log.d("PLAYBACK", "PAUSED")
-                mediaPlayer.pause()
-                updatePlaybackState(PlaybackState.STATE_PAUSED)
-            }
-            ACTION_PLAY_PAUSE -> {
-                // Handle play action
-                Log.d("PLAYBACK", "PLAY")
-                mediaPlayer.start()
-                updatePlaybackState(PlaybackState.STATE_PLAYING)
-            }
-            ACTION_NEXT -> {
-                // Handle next action
-                // ...
-                Log.d("PLAYBACK", "NEXT")
-            }
-            ACTION_PREV -> {
-                // Handle previous action
-                // ...
-                Log.d("PLAYBACK", "PREV")
-            }
-            else -> {
-                // Handle other cases
-                Log.d("PLAYBACK", "FUKCU")
-            }
-        }
-
         val notificationManager : NotificationManager = applicationContext.getSystemService(
             NOTIFICATION_SERVICE
         ) as NotificationManager
@@ -97,24 +82,6 @@ class MoaiRadioService : Service(), OnPreparedListener {
             applicationContext.resources,
             R.drawable.moai_radio_stylized
         )
-
-        val metadataBuilder = MediaMetadataCompat.Builder().apply {
-            // To provide most control over how an item is displayed set the
-            // display fields in the metadata
-            putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_TITLE, "Prova")
-            //putString(MediaMetadata.METADATA_KEY_DISPLAY_SUBTITLE, myData.displaySubtitle)
-            //putString(MediaMetadata.METADATA_KEY_DISPLAY_ICON_URI, myData.artUri)
-            // And at minimum the title and artist for legacy support
-            putString(MediaMetadataCompat.METADATA_KEY_TITLE, "Prova")
-            //putString(MediaMetadata.METADATA_KEY_ARTIST, myData.artist)
-            // A small bitmap for the artwork is also recommended
-            //putBitmap(MediaMetadata.METADATA_KEY_ART, myData.artBitmap)
-            // Add any other fields you have for your data as well
-            putLong(MediaMetadataCompat.METADATA_KEY_DURATION, 197000)
-        }
-
-        mediaSession?.setMetadata(metadataBuilder.build())
-        mediaSession?.isActive = true
 
         val playPauseAction: NotificationCompat.Action = NotificationCompat.Action.Builder(
             R.drawable.ic_baseline_pause_24, "Pause",
@@ -173,6 +140,7 @@ class MoaiRadioService : Service(), OnPreparedListener {
             )
             .build()
 
+        updateMetadata()
         mediaSession!!.setPlaybackState(playbackState)
         MediaButtonReceiver.handleIntent(mediaSession, intent)
 
@@ -181,23 +149,117 @@ class MoaiRadioService : Service(), OnPreparedListener {
 
         startForeground(ONGOING_NOTIFICATION_ID, notification)
 
-        mediaPlayer.setDataSource("https://moai.eu.pythonanywhere.com/".plus(currentSongId.toString())) // METTERE RANDOM
+        mediaPlayer.setDataSource("https://moai.eu.pythonanywhere.com/".plus(playlistRadio?.get(currentSongId).toString()))
+
         mediaPlayer.setOnPreparedListener(this)
         mediaPlayer.prepareAsync()
+
+        mediaSession!!.setCallback(object : MediaSessionCompat.Callback() {
+                override fun onPlay() {
+                    super.onPlay()
+                    mediaPlayer.start()
+                    updatePlaybackState(PlaybackState.STATE_PLAYING)
+                }
+
+                override fun onPause() {
+                    super.onPause()
+                    mediaPlayer.pause()
+                    updatePlaybackState(PlaybackState.STATE_PAUSED)
+                }
+
+                override fun onSkipToNext() {
+                    super.onSkipToNext()
+                    skipSong()
+                }
+
+                override fun onSkipToPrevious() {
+                    super.onSkipToPrevious()
+                    mediaPlayer.stop()
+                    mediaPlayer.reset()
+                    updatePlaybackState(PlaybackState.STATE_STOPPED)
+                    if (currentSongId == 0) {
+                        currentSongId = numSong - 1
+                    } else {
+                        currentSongId--
+                    }
+                    mediaPlayer.setDataSource("https://moai.eu.pythonanywhere.com/".plus(playlistRadio?.get(currentSongId).toString())) // METTERE RANDOM
+                    mediaPlayer.prepareAsync()
+                    updateMetadata()
+                }
+
+                override fun onSeekTo(pos: Long) {
+                    super.onSeekTo(pos)
+                    mediaPlayer.seekTo(pos.toInt())
+                    updatePlaybackState(PlaybackState.STATE_PLAYING)
+                }
+            }
+        )
+
+        mediaSession?.isActive = true
 
         return startMode
     }
 
+    private fun skipSong() {
+        mediaPlayer.stop()
+        mediaPlayer.reset()
+        updatePlaybackState(PlaybackState.STATE_STOPPED)
+        currentSongId = (currentSongId + 1) % numSong
+        mediaPlayer.setDataSource("https://moai.eu.pythonanywhere.com/".plus(playlistRadio?.get(currentSongId).toString()))
+        mediaPlayer.prepareAsync()
+        updateMetadata()
+    }
+
     private fun updatePlaybackState(state: Int) {
-        Log.d("PLAYBACK", "CIAO")
         val stateBuilder = PlaybackStateCompat.Builder()
         stateBuilder.setActions(
             PlaybackStateCompat.ACTION_PLAY_PAUSE or
                     PlaybackStateCompat.ACTION_SKIP_TO_NEXT or
-                    PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+                    PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS or
+                    PlaybackStateCompat.ACTION_SEEK_TO
         )
         stateBuilder.setState(state, mediaPlayer.currentPosition.toLong(), 1.0f)
         mediaSession?.setPlaybackState(stateBuilder.build())
+    }
+
+    fun updateMetadata() {
+        var info : Song? = null
+        var response : Response? = null
+        scope.launch {
+            val client = OkHttpClient()
+
+            val url: HttpUrl = HttpUrl.Builder()
+                .scheme("https")
+                .host("moai.eu.pythonanywhere.com")
+                .addPathSegment("info")
+                .addQueryParameter("id", playlistRadio?.get(currentSongId).toString())
+                .build()
+
+            val requesthttp: Request = Request.Builder()
+                .addHeader("accept", "application/json")
+                .url(url)
+                .build()
+
+            response = client.newCall(requesthttp).execute()
+
+        }.invokeOnCompletion {
+            info = Gson().fromJson(response?.body?.string(), Song::class.java)
+
+            val metadataBuilder = MediaMetadataCompat.Builder().apply {
+                info?.let { it ->
+                    putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_TITLE, it.name)
+                    putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_SUBTITLE, it.artist)
+                    putString(MediaMetadataCompat.METADATA_KEY_TITLE, it.name)
+                    putString(MediaMetadataCompat.METADATA_KEY_ARTIST, it.artist)
+                }
+                info?.duration?.let { it ->
+                    putLong(MediaMetadataCompat.METADATA_KEY_DURATION,
+                        it
+                    )
+                }
+            }
+            mediaSession?.setMetadata(metadataBuilder.build())
+        }
     }
 
     override fun onDestroy() {
@@ -206,10 +268,7 @@ class MoaiRadioService : Service(), OnPreparedListener {
     }
 
     override fun onPrepared(mp: MediaPlayer?) {
-        startRadio()
-    }
-
-    private fun startRadio() {
         mediaPlayer.start()
+        updatePlaybackState(PlaybackState.STATE_PLAYING)
     }
 }
